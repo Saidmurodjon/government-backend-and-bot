b7098afe3a05995a946f03ec607182b56cec963f
kali <kali@kali.kali>
2022-06-17 23:25:15 +0500

    d

--- users/user.model.js
+++ users/user.model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const usersSchema = mongoose.Schema({
     tash:String,
-    lavoz:String,
+    lavozim:String,
     fish:String,
     ismi:String,
     tel:String,
